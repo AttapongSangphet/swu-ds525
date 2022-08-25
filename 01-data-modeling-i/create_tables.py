de48fa416399a14104fa75be995ed0e3518b3140
AttapongSangphet <attapong.sangphet@g.swu.ac.th>
2022-08-25 17:26:31 +0000

    Modified to create tables

--- 01-data-modeling-i/create_tables.py
+++ 01-data-modeling-i/create_tables.py
@@ -1,11 +1,50 @@
 import psycopg2
 
 
-table_drop = "DROP TABLE IF EXISTS songplays"
+table_drop = "DROP TABLE IF EXISTS Event, Actor, Repo, Org"
 
-table_create = """
-    CREATE TABLE IF NOT EXISTS xxx (
-    )
+table_create = """ 
+    CREATE TABLE IF NOT EXISTS Event (
+        E_id int NOT NULL,
+        R_id int NOT NULL,
+        O_id int NOT NULL,
+        A_id int NOT NULL,
+        E_type varchar(250),
+        E_Public varchar(250),
+        E_create_at varchar(250),
+        PRIMARY KEY (E_id)
+    );
+    CREATE TABLE IF NOT EXISTS Actor (
+        A_id int NOT NULL,
+        login varchar(250),
+        display_login varchar(250),
+        A_gravatar_id integer,
+        A_url varchar(250),
+        A_avatar_url varchar(250),
+        PRIMARY KEY (A_id)
+    );
+    CREATE TABLE IF NOT EXISTS Org (
+        O_id int NOT NULL,
+        login varchar(250),
+        O_gravatar_id integer,
+        O_url varchar(250),
+        O_avatar_url varchar(250),
+        PRIMARY KEY (O_id)
+    );
+    CREATE TABLE IF NOT EXISTS Repo (
+        R_id int NOT NULL,
+        R_name varchar(250),
+        R_url varchar(250),
+        PRIMARY KEY (R_id)
+    );
+    ALTER TABLE Event
+    ADD FOREIGN KEY (R_id) REFERENCES Repo(R_id);
+
+    ALTER TABLE Event
+    ADD FOREIGN KEY (O_id) REFERENCES Org(O_id);
+
+    ALTER TABLE Event
+    ADD FOREIGN KEY (A_id) REFERENCES Actor(A_id);
 """
 
 create_table_queries = [
@@ -16,7 +55,7 @@ drop_table_queries = [
 ]
 
 
-def drop_tables(cur: PostgresCursor, conn: PostgresConn) -> None:
+def drop_tables(cur, conn) -> None:
     """
     Drops each table using the queries in `drop_table_queries` list.
     """
@@ -25,7 +64,7 @@ def drop_tables(cur: PostgresCursor, conn: PostgresConn) -> None:
         conn.commit()
 
 
-def create_tables(cur: PostgresCursor, conn: PostgresConn) -> None:
+def create_tables(cur, conn) -> None:
     """
     Creates each table using the queries in `create_table_queries` list.
     """
