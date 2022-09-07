9811ec62ad0de4679d45ca3933d7f2f261751231
unknown <attapong.sangphet@g.swu.ac.th>
2022-09-08 01:36:54 +0700

    revised

--- 02-data-modeling-ii/etl.py
+++ 02-data-modeling-ii/etl.py
@@ -68,7 +68,7 @@ def process(session, filepath):
     all_files = get_files(filepath)
 
     for datafile in all_files:
-        with open(datafile, "r") as f:
+        with open(datafile, "r", encoding="utf-8") as f:
             data = json.loads(f.read())
             for each in data:
                 # Print some sample data
